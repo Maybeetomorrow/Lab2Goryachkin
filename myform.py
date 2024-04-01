3bf261286c61534500fa26901d0d6e0fcbf2364e
uurixan <wp31012000@gmail.com>
2024-04-01 22:05:22 +0300

    Add date check

--- myform.py
+++ myform.py
@@ -1,5 +1,6 @@
 from bottle import post, request
 import re
+from datetime import datetime
 
 # Паттерн для проверки адреса электронной почты
 email_pattern = re.compile(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
@@ -10,16 +11,27 @@ def my_form():
     quest = request.forms.get('QUEST')  # Получаем вопрос
     name = request.forms.get('NAME')    # Получаем имя
     mail = request.forms.get('ADRESS')  # Получаем адрес электронной почты
-    date = request.forms.get('DATE')    # Получаем дату
+    date_str = request.forms.get('DATE')    # Получаем дату в виде строки
 
     # Проверяем, заполнены ли все поля формы
-    if not (quest and name and mail and date):
+    if not (quest and name and mail and date_str):
         return "Please fill in all fields."
 
     # Проверяем, соответствует ли адрес электронной почты заданному шаблону
-    if email_pattern.match(mail):
-        # Если адрес электронной почты действителен, возвращаем сообщение об успешной отправке формы
-        return "Thanks %s! The answer will be sent to the email %s Access Date: %s" % (name, mail, date)
-    else:
-        # Если адрес электронной почты недействителен, возвращаем сообщение об ошибке
+    if not email_pattern.match(mail):
         return "Invalid email address. Please enter a valid email address."
+
+    # Проверяем введенную дату
+    try:
+        # Преобразуем строку даты в объект datetime
+        date = datetime.strptime(date_str, "%Y-%m-%d")
+        # Получаем текущую дату
+        current_date = datetime.now().date()
+        # Проверяем, что введенная дата не больше текущей даты
+        if date > current_date:
+            return "Date cannot be greater than current date."
+    except ValueError:
+        return "Invalid date format. Please enter date in YYYY-MM-DD format."
+
+    # Если все проверки пройдены успешно, возвращаем сообщение об успешной отправке формы
+    return "Thanks %s! The answer will be sent to the email %s Access Date: %s" % (name, mail, date_str)
