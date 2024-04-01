3e73c68f21fe7b4bc800251ce0c379aeec794539
uurixan <wp31012000@gmail.com>
2024-04-01 21:29:11 +0300

    Add date

--- myform.py
+++ myform.py
@@ -8,8 +8,9 @@ email_pattern = re.compile(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
 def my_form():
     mail = request.forms.get('ADRESS')
     name = request.forms.get('NAME')
+    date = request.forms.get('DATE')
 
     if email_pattern.match(mail):
-        return "Thanks %s! The answer will be sent to the email %s" % (name, mail)
+        return "Thanks %s! \nThe answer will be sent to the email %s \nAccess Date: %s" % (name, mail, date)
     else:
         return "Invalid email address. Please enter a valid email address."
