e6d2947ac86015891d89e8ab912639f69df7a7ff
uurixan <wp31012000@gmail.com>
2024-04-01 21:32:30 +0300

    Add check fields

--- myform.py
+++ myform.py
@@ -6,11 +6,15 @@ email_pattern = re.compile(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
 
 @post('/home')
 def my_form():
-    mail = request.forms.get('ADRESS')
+    quest = request.forms.get('QUEST')
     name = request.forms.get('NAME')
+    mail = request.forms.get('ADRESS')
     date = request.forms.get('DATE')
 
+    if not (quest and name and mail and date):
+        return "Please fill in all fields."
+
     if email_pattern.match(mail):
-        return "Thanks %s! \nThe answer will be sent to the email %s \nAccess Date: %s" % (name, mail, date)
+        return "Thanks %s! The answer will be sent to the email %s Access Date: %s" % (name, mail, date)
     else:
         return "Invalid email address. Please enter a valid email address."
