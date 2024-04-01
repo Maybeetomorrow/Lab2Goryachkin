a09ebf34bace429956e1a76703fbbe845c8458a7
uurixan <wp31012000@gmail.com>
2024-04-01 21:26:40 +0300

    Add email pattern and user thanks

--- myform.py
+++ myform.py
@@ -1,6 +1,15 @@
 from bottle import post, request
+import re
+
+# Паттерн для проверки адреса электронной почты
+email_pattern = re.compile(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
 
 @post('/home')
 def my_form():
     mail = request.forms.get('ADRESS')
-    return "Thanks! The answer will be sent to the mail %s" % mail
+    name = request.forms.get('NAME')
+
+    if email_pattern.match(mail):
+        return "Thanks %s! The answer will be sent to the email %s" % (name, mail)
+    else:
+        return "Invalid email address. Please enter a valid email address."
